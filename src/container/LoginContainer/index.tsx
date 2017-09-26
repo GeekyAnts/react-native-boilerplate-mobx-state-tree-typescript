8e701f338cd82a287946de06e2b3041dad8c0581
Shivraj <shivraj@geekyants.com>
2017-09-26 12:24:31 +0530

    login story changes

--- src/container/LoginContainer/index.tsx
+++ src/container/LoginContainer/index.tsx
@@ -1,6 +1,6 @@
 // @flow
 import * as React from "react";
-import { Item, Input, Icon, View, Text, Toast } from "native-base";
+import { Form, Item, Input, Toast, Icon } from "native-base";
 import { observer, inject } from "mobx-react/native";
 
 import Login from "../../stories/screens/Login";
@@ -23,7 +23,7 @@ export default class LoginContainer extends React.Component<Props, State> {
 			this.props.navigation.navigate("Drawer");
 		} else {
 			Toast.show({
-				text: "Enter Valid Email & password!",
+				text: "Please enter valid credentials",
 				duration: 2000,
 				position: "top",
 				textStyle: { textAlign: "center" },
@@ -33,55 +33,33 @@ export default class LoginContainer extends React.Component<Props, State> {
 	render() {
 		const form = this.props.loginForm;
 		const Fields = (
-			<View>
-				<View>
-					<Item error={form.emailError ? true : false}>
-						<Icon active name="person" />
-						<Input
-							placeholder="EMAIL"
-							keyboardType="email-address"
-							ref={c => (this.emailInput = c)}
-							value={form.email}
-							onBlur={() => form.validateEmail()}
-							onChangeText={e => form.emailOnChange(e)}
-						/>
-						{form.emailError ? (
-							<Icon active onPress={() => this.emailInput._root.clear()} name="close" />
-						) : (
-							<Text />
-						)}
-					</Item>
-					{form.emailError ? (
-						<Text style={{ color: "red", fontSize: 13, textAlign: "right" }}>{form.emailError}</Text>
-					) : (
-						<Text style={{ color: "transparent", fontSize: 13, textAlign: "right" }}>No error</Text>
-					)}
-				</View>
-				<View>
-					<Item error={form.passwordError ? true : false}>
-						<Icon active name="unlock" />
-						<Input
-							placeholder="PASSWORD"
-							ref={c => (this.pwdinput = c)}
-							value={form.password}
-							onBlur={() => form.validatePassword()}
-							onChangeText={e => form.passwordOnChange(e)}
-							secureTextEntry={true}
-						/>
-						{form.passwordError ? (
-							<Icon active onPress={() => this.pwdinput._root.clear()} name="close" />
-						) : (
-							<Text />
-						)}
-					</Item>
-					{form.passwordError ? (
-						<Text style={{ color: "red", fontSize: 13, textAlign: "right" }}>{form.passwordError}</Text>
-					) : (
-						<Text style={{ color: "transparent", fontSize: 13, textAlign: "right" }}>No error</Text>
-					)}
-				</View>
-			</View>
+			<Form>
+				<Item error={form.emailError ? true : false}>
+					<Icon active name="person" />
+					<Input
+						placeholder="Username"
+						keyboardType="email-address"
+						ref={c => (this.emailInput = c)}
+						value={form.email}
+						onBlur={() => form.validateEmail()}
+						onChangeText={e => form.emailOnChange(e)}
+					/>
+				</Item>
+
+				<Item error={form.passwordError ? true : false} last>
+					<Icon active name="unlock" />
+					<Input
+						placeholder="Password"
+						ref={c => (this.pwdinput = c)}
+						value={form.password}
+						onBlur={() => form.validatePassword()}
+						onChangeText={e => form.passwordOnChange(e)}
+						secureTextEntry={true}
+					/>
+				</Item>
+			</Form>
 		);
+
 		return <Login loginForm={Fields} onLogin={() => this.login()} />;
 	}
 }
